1c79fe6fd64918b908eed047548c86508b1e00ab
Nanang Endrik S <nanangendriks@gmail.com>
2021-07-17 22:54:29 +0700

    added tailwind-rn

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Platform} from 'react-native'
+import tailwind from 'tailwind-rn'
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={tailwind('bg-blue-100 p-4 flex-1')}>
+      <Text style={tailwind('text-lg font-semibold')}>Hello from {Platform.OS}</Text>
     </View>
   )
 }
