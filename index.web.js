b717fffd4a76a8a8012bcc0552118936010b4654
Nanang Endrik S <nanangendriks@gmail.com>
2021-07-17 22:53:14 +0700

    added react-native-web

---
+++ index.web.js
@@ -0,0 +1,14 @@
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+
+if (module.hot) {
+    module.hot.accept();
+}
+
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+    initialProps: {},
+    rootTag: document.getElementById('root'),
+});
