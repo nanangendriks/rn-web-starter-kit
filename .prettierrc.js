eb6494795b683944153d069441a024a8e12a7047
Nanang Endrik S <nanangendriks@gmail.com>
2021-10-18 15:27:21 +0700

    change prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
   arrowParens: 'avoid',
+  endOfLine: 'auto',
 };
